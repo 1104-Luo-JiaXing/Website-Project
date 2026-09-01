bd8275a0e8cf021f38fc2675f91c1ea6412bd503
1104-Luo-JiaXing <jiaxingl@unr.edu>
2026-09-01 16:50:54 -0700

    Added frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold text-blue-600">Tailwind test</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
